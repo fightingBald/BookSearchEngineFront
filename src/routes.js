3879d9548b0e90f94868790fef44066d1fe3b0bd
aziz-abderrahmen <aziz.abderrahmen.mtc@gmail>
2023-02-03 13:10:36 +0100

    Update route

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,12 @@ import { Route, Switch } from 'react-router-dom';
 // Configure routes
 import Home from './screens/home';
 import Book from './screens/book';
+import Recommendations from './screens/recommendations';
 
 export default (
   <Switch>
     <Route exact path="/" component={Home}/>
     <Route path="/book/:ID" component={Book}/>
-    <Route path="/recommendation/:" component={Book}/>
+    <Route path="/recommendation/:" component={Recommendations}/>
   </Switch>
 );
